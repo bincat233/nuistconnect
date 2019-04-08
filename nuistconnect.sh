c6ed5e9fd76149d2dd15c82d90aa18772db5b2b7
熊子稼 <897670526@qq.com>
2019-04-08 22:04:52 +0800

    Improved automatic login process.

--- nuistconnect.sh
+++ nuistconnect.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 triggerssid="i-NUIST"
-ssid=$(networksetup -getairportnetwork en0 | awk -F": " '{print $2}')
 username="guest"
 domain="NUIST"
 password="Z3Vlc3Q="
+ssid=$(networksetup -getairportnetwork en0 | awk -F": " '{print $2}')
 
 #echo "username=${username}&domain=${domain}&password=${password}"
 #originpwd="guest"
 #password=$(echo -n "${originpwd}" | base64)
-echo ${password}
 if [ "$ssid" == "$triggerssid" ]; then
-  curl -m 20 -s -d "username=${username}&domain=${domain}&password=${password}" http://10.255.255.13/index.php/index/login
+  # Waiting for network
+  until ping -nq -c3 10.255.255.13; do
+    sleep 1
+  done
+
+  # Login
+  curl -vs -d "username=${username}&domain=${domain}&password=${password}" http://10.255.255.13/index.php/index/login
+
+  # Waiting for captive portal and kill it
+  until ps aux | grep Captive | grep -qv grep; do
+    sleep 1
+  done
+  killall "Captive Network Assistant"
 fi
